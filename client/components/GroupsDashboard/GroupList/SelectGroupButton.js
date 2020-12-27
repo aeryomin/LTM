66b8faa55bcd6adb82fa7cd1923bd3727759c795
aeryomin <eryomin.developer@gmail.com>
2020-12-27 23:00:26 +0300

    fix grouplist styles

--- client/components/GroupsDashboard/GroupList/SelectGroupButton.js
+++ client/components/GroupsDashboard/GroupList/SelectGroupButton.js
@@ -14,10 +14,10 @@ const SelectGroupButton = (props) => {
       type="button"
       className={`flex flex-col w-4/6 space-y-1 border rounded pl-2 text-left
         ${ui.groups.SelectButton.active.bgColor}
-        ${ui.groups.SelectButton.active.FontSize}
-        ${ui.groups.SelectButton.active.FontColor}
+        ${ui.groups.SelectButton.active.fontSize}
+        ${ui.groups.SelectButton.active.fontColor}
         hover:${ui.groups.SelectButton.hover.bgColor}
-        hover:${ui.groups.SelectButton.hover.FontColor}`}
+        hover:${ui.groups.SelectButton.hover.fontColor}`}
       onClick={() => {
         props.setSelectedGroupID(props.group._id)
         dispatch(setActiveGroupID(props.group._id))
